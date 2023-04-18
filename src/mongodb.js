f3908de271370acf3ce9972b3aafd0897258a41f
faqih555 <m.faqihabdilah@gmail.com>
2023-04-18 12:36:11 +0800

    mengconnectkan ke mongodb atlas

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/FrogRes")
+mongoose.connect("mongodb+srv://faqih:gantinama@frog-res.7orxjmh.mongodb.net/frog-res")
 .then(() =>{
     console.log("mongodb conected")
 })
