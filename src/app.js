cb2db2b7307ce4a1782b3b867b49de2bc9b652a8
faqih555 <m.faqihabdilah@gmail.com>
2023-04-20 23:30:13 +0800

    membuat kehampaan

--- src/app.js
+++ src/app.js
@@ -1,143 +1,142 @@
 require('dotenv').config()
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const { body, validationResult, check } = require("express-validator");
-const {collection, upload} = require("./mongodb");
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+const { body, validationResult, check } = require("express-validator")
+const {collection, upload} = require("./mongodb")
 const bcryptjs = require('bcryptjs')
 const jsonwebtoken = require('jsonwebtoken')
-const session = require("express-session");
-const app = express();
-const port = 3000;
+const session = require("express-session")
+const app = express()
+const port = 3000
 
 // middleware
-app.use(express.json());
-app.use(expressLayouts);
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(expressLayouts)
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: false }))
 app.use(
   session({
     secret: "secret-key", // Kunci rahasia untuk menyimpan session
     resave: false,
     saveUninitialized: false,
   })
-);
+)
 
 app.get("/", (req, res) => {
   res.render("index", {
     layout: "layouts/main-layouts",
-  });
-});
+  })
+})
 
 app.get("/ebook", (req, res) => {
   res.render("ebook", {
     layout: "layouts/main-layouts",
-  });
-});
+  })
+})
 
 app.get("/login", (req, res) => {
   res.render("login", {
     layout: "layouts/main-layouts",
-  });
-});
+  })
+})
 
 app.get("/register", (req, res) => {
   res.render("register", {
     layout: "layouts/main-layouts",
-  });
-});
+  })
+})
 
 app.post(
   "/register",
   [
     body("name").custom(async (value) => {
       if (value == "") {
-        throw new Error("nama tidak boleh kosong");
+        throw new Error("nama tidak boleh kosong")
       }
-      return true;
+      return true
     }),
     body("email").custom(async (value) => {
       if (value == "") {
-        throw new Error("email tidak boleh kosong");
+        throw new Error("email tidak boleh kosong")
       }
-      return true;
+      return true
     }),
     body("password").custom(async (value) => {
       if (value == "") {
-        throw new Error("password tidak boleh kosong");
+        throw new Error("password tidak boleh kosong")
       }
-      return true;
+      return true
     }),
     body("name").custom(async (value) => {
-      const duplikat = await collection.findOne({ name: value });
+      const duplikat = await collection.findOne({ name: value })
       if (duplikat) {
-        throw new Error("nama sudah terdaftar");
+        throw new Error("nama sudah terdaftar")
       }
       return true;
     }),
     body("email").custom(async (value) => {
-      const duplikat = await collection.findOne({ email: value });
+      const duplikat = await collection.findOne({ email: value })
       if (duplikat) {
-        throw new Error("email sudah terdaftar");
+        throw new Error("email sudah terdaftar")
       }
-      return true;
+      return true
     }),
     check("email", "invalid email address").isEmail(),
     check("password", "minimum password legth is 8 characters").isLength({ min: 8 }),
   ],
   async (req, res) => {
-    const { name, email, password } = req.body;
-    const hashedPassword = await bcryptjs.hash(password, 10); // Melakukan hashing password dengan bcryptjs
+    const { name, email, password } = req.body
+    const hashedPassword = await bcryptjs.hash(password, 10) // Melakukan hashing password dengan bcryptjs
     const data = {
       name: name,
       email: email,
       password: hashedPassword, // Menggunakan password yang sudah di-hash
     };
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      // return res.status(400).json({ errors: errors.array() });
       res.render("register", {
         layout: "layouts/main-layouts",
         errors: errors.array(),
-      });
+      })
     } else {
-      await collection.insertMany([data]);
+      await collection.insertMany([data])
 
       res.render("login", {
         layout: "layouts/main-layouts",
-      });
+      })
     }
   }
-);
+)
 
 app.post(
   "/login",
   [
     body("name").custom(async (value) => {
       if (value == "") {
-        throw new Error("nama tidak boleh kosong");
+        throw new Error("nama tidak boleh kosong")
       }
       return true;
     }),
     body("password").custom(async (value) => {
       if (value == "") {
-        throw new Error("password tidak boleh kosong");
+        throw new Error("password tidak boleh kosong")
       }
-      return true;
+      return true
     }),
     body("name").custom(async (value) => {
-      const user = await collection.findOne({ name: value });
+      const user = await collection.findOne({ name: value })
       if (!user) {
-        throw new Error("nama belum terdaftar");
+        throw new Error("nama belum terdaftar")
       }
       return true;
     }),
     body("password").custom(async (value) => {
-      const user = await collection.findOne({ password: value });
+      const user = await collection.findOne({ password: value })
       if (!user) {
-        throw new Error("password anda salah");
+        throw new Error("password anda salah")
       }
-      return true;
+      return true
     }),
   ],
   async (req, res) => {
@@ -154,10 +153,10 @@ app.post(
         return res.render("index", {
           layout: "layouts/main-layouts",
           token: token
-        });
+        })
       }
     }
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
       // return res.status(400).json({ errors: errors.array() });
       res.render("login", {
@@ -380,7 +379,7 @@ app.post("/upload", async (req, res) => {
     konten,
     source: source,
   };
-  const errors = validationResult(req);
+  const errors = validationResult(req)
   if (!errors.isEmpty()) {
     // return res.status(400).json({ errors: errors.array() });
     const artikel = await upload.findOne({source})
@@ -393,7 +392,7 @@ app.post("/upload", async (req, res) => {
       errors: errors.array(),
     });
   } else {
-    await upload.insertMany([data]);
+    await upload.insertMany([data])
     const artikels = await upload.find({source}).lean()
     const artikel = await upload.findOne({source})
     res.render(source, {
@@ -402,9 +401,9 @@ app.post("/upload", async (req, res) => {
       source,
       artikel,
       artikels,
-    });
+    })
   }
-});
+})
 
 app.get('/:source/:judul', async (req, res) => {
   const source = req.params.source || req.session.source // mengambil nilai dari parameter path atau dari req.session.source jika tidak ada parameter path
@@ -418,13 +417,13 @@ app.get('/:source/:judul', async (req, res) => {
     artikel,
     artikels,
   })
-});
+})
 
 app.use("/", (req, res) => {
-  res.status(404);
-  res.send("<h1> 404 <h1>");
-});
+  res.status(404)
+  res.send("<h1> 404 <h1>")
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
